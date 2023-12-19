7354f00d54275170f93adaafe3bd430a305675ca
JustFiesta <mateusz.bocak9@gmail.com>
2023-12-19 22:36:27 +0100

    Locked size

--- src/main/java/com/example/parkomania/HelloApplication.java
+++ src/main/java/com/example/parkomania/HelloApplication.java
@@ -1,5 +1,6 @@
 package com.example.parkomania;
 
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -10,10 +11,11 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("mainMenu.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("bookPlace.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("ParkoMania");
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 

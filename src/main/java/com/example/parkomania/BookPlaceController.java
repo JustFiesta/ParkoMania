27790e7049657554a716fe65f234fa34ea77743a
JustFiesta <mateusz.bocak9@gmail.com>
2023-12-19 22:25:25 +0100

    Final place booking scene and controller sketch

---
+++ src/main/java/com/example/parkomania/BookPlaceController.java
@@ -0,0 +1,23 @@
+package com.example.parkomania;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class BookPlaceController {
+    @FXML
+    private Label choosingTimer;
+
+    @FXML
+    private Button startAndStopBtn;
+    @FXML
+    private ImageView startAndStopImg;
+
+    @FXML
+    private void startCounting(ActionEvent event){
+//        startAndStopImg.setImage(new Image("C:\\Users\\Masti\\Desktop\\Programowanie\\Java\\ParkoMania\\src\\main\\resources\\com\\example\\parkomania\\images\\start.png"));
+    }
+}

8cc4fd81a62378e4cc6f8b124a4c1fa364f5b197
JustFiesta <mateusz.bocak9@gmail.com>
2023-12-21 13:52:49 +0100

    history of reservations with controller

--- src/main/java/com/example/parkomania/HistoryController.java
+++ src/main/java/com/example/parkomania/HistoryController.java
@@ -3,13 +3,15 @@ package com.example.parkomania;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
 
 import static com.example.parkomania.HelloApplication.sceneManager;
 
 public class HistoryController {
     @FXML
     private Button goBack;
-
+    @FXML //Reservation
+    private TableView<?> historyTable;
     @FXML
     void goBack(ActionEvent event) {
         sceneManager.switchScene("account");

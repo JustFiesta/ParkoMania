a14b49f7685a415909d237b6b52c72e9284d07af
JustFiesta <mateusz.bocak9@gmail.com>
2023-12-21 13:17:59 +0100

    basic controlls in user data scene

--- src/main/java/com/example/parkomania/UserDataController.java
+++ src/main/java/com/example/parkomania/UserDataController.java
@@ -3,13 +3,44 @@ package com.example.parkomania;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 
 import static com.example.parkomania.HelloApplication.sceneManager;
 
 public class UserDataController {
+
+    @FXML
+    private Label email;
+
     @FXML
     private Button goBack;
 
+    @FXML
+    private Button goToAddVehicle;
+
+    @FXML
+    private Button goToChangePassword;
+
+    @FXML
+    private Label nrTel;
+
+    @FXML
+    private HBox vehicleContainer;
+
+    @FXML
+    private Label vehicleRegistration;
+
+
+    @FXML
+    void goToAddVehicle(ActionEvent event) {
+        sceneManager.switchScene("addVehicle");
+    }
+
+    @FXML
+    void goToChangePassword(ActionEvent event) {
+        sceneManager.switchScene("changePassword");
+    }
     @FXML
     void goBack(ActionEvent event) {
         sceneManager.switchScene("account");

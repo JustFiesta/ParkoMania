a39593313e2a5ff5b34984f4553417b06c1a815d
JustFiesta <mateusz.bocak9@gmail.com>
2024-01-02 17:38:17 +0100

    New Launcher class for jar file

---
+++ src/main/java/com/example/parkomania/Launcher.java
@@ -0,0 +1,8 @@
+package com.example.parkomania;
+
+// need for jar bcs it doesnt inherit anything
+public class Launcher {
+    public static void main(String[] args) {
+        HelloApplication.main(args);
+    }
+}

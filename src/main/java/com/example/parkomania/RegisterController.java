af464ae5b88f93d471a4e93bb4dc2f8b9ac614f8
JustFiesta <mateusz.bocak9@gmail.com>
2024-01-02 17:35:32 +0100

    Submitting new user without form - debug test

--- src/main/java/com/example/parkomania/RegisterController.java
+++ src/main/java/com/example/parkomania/RegisterController.java
@@ -4,6 +4,10 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
 
 import static com.example.parkomania.HelloApplication.sceneManager;
 
@@ -45,8 +49,30 @@ public class RegisterController {
     }
 
     @FXML
-    void subtNewUser(ActionEvent event) {
+    void submittNewUser(ActionEvent event) {
+        User newUser = new User(0, "543534543", "chuj@chuj.gmail.com", "easweasbgera2q3", UserType.USER);
 
+        //create session factory
+        SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+        Session session = sessionFactory.openSession();
+
+        Transaction transaction = null;
+
+        try {
+            transaction = session.beginTransaction();
+            session.persist(newUser);
+            transaction.commit();
+            System.out.println("New user added");
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            System.out.println("Something went wrong");
+            e.printStackTrace();
+        } finally {
+            session.close();
+            System.out.println("Close");
+        }
+        sessionFactory.close();
     }
-
 }

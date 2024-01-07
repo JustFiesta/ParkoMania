fe4e9f1013b6106dbf373afe6a2b761cdbf60f09
JustFiesta <mateusz.bocak9@gmail.com>
2024-01-07 21:07:49 +0100

    log in function and password check sketch

--- src/main/java/com/example/parkomania/LoginController.java
+++ src/main/java/com/example/parkomania/LoginController.java
@@ -5,6 +5,13 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import java.util.List;
+import java.util.regex.Pattern;
 
 import static com.example.parkomania.HelloApplication.sceneManager;
 
@@ -22,15 +29,13 @@ public class LoginController {
     private TextField phoneField;
 
     @FXML
-    void checkPassword(ActionEvent event) {
-
-    }
+    private Label uncorrectNumberFormat;
 
     @FXML
-    void checkPhoneNum(ActionEvent event) {
+    private Label phoneNotFound;
 
-    }
-
+    @FXML
+    private Label incorrectPassword;
 
     @FXML
     void goBack(ActionEvent event) {
@@ -39,12 +44,113 @@ public class LoginController {
 
     @FXML
     void logIn(ActionEvent event) {
-        System.out.println("Checking database");
-//        if (success){
-//            create new user singleton
-//        }else {
-//            somtehing went wrong, try again
-//        }
+        System.out.println("Debug: Credentails submitted");
+        if (checkCredentials(phoneField.getText(), passwordField.getText())) {
+        sceneManager.switchScene("mainMenu");
+//            create new user singleton,
+//            go to main menu
+        }else {
+            System.out.println("Debug: Phone or password is wrong");
+        }
     }
 
+    boolean checkCredentials(String phoneNumber, String password) {
+        //check if phone number is a number (doesnt contain any chars)
+
+        //regular expression for phone number
+        String phoneRegex = "^[0-9\\-\\s]+$";
+
+        //compile regular expression
+        Pattern pattern = Pattern.compile(phoneRegex);
+
+        //check if it matches - if not show message and clear phone field
+        if (!pattern.matcher(phoneNumber).matches()) {
+            uncorrectNumberFormat.setVisible(true);
+            phoneField.clear();
+            return false;
+        }else {
+            uncorrectNumberFormat.setVisible(false);
+        }
+
+        //if password is empty do not check db
+        if (password.isEmpty()){
+            incorrectPassword.setVisible(true);
+            return false;
+        }else {
+            incorrectPassword.setVisible(false);
+        }
+
+        //check if we have same number in database - HQL needed. SQL throws exceptions
+        String checkPhoneNumInDBQuery = "SELECT phone_number FROM User WHERE phone_number = :phoneNumber";
+        String checkPasswordHashInDBQuery = "SELECT password_hash FROM User WHERE phone_number = :phoneNumber";
+
+        SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+        Session session = sessionFactory.openSession();
+
+        Transaction transaction = null;
+
+        //try to send transaction to db
+        try {
+            transaction = session.beginTransaction();
+            List<String> phoneNumbers = session.createQuery(checkPhoneNumInDBQuery)
+                    .setParameter("phoneNumber", phoneNumber)
+                    .getResultList();
+            List<String> passwordHashes = session.createQuery(checkPasswordHashInDBQuery)
+                    .setParameter("phoneNumber", phoneNumber)  // Ustawienie tego samego parametru 'phoneNumber'
+                    .getResultList();
+
+            if (!phoneNumbers.isEmpty()) {
+//                /// found number, so check if password matches
+                String storedPasswordHash = (String) passwordHashes.get(0); // assuming the second column is the password hash
+
+                if (checkPassword(password, storedPasswordHash)) {
+                    // password is correct
+                    session.close();
+                    sessionFactory.close();
+
+                    return true;
+                } else {
+                    // incorrect password
+                    incorrectPassword.setVisible(true);
+                }
+            }else {
+                System.out.println("Debug: No phone numbers found.");
+                phoneNotFound.setVisible(true);
+            }
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            System.out.println("Debug: Something went wrong");
+            e.printStackTrace();
+        } finally {
+            session.close();
+            sessionFactory.close();
+            System.out.println("Debug: Session close");
+        }
+
+        return false;
+    }
+
+    //hash given password and compare it to password in db
+    boolean checkPassword(String inputPassword, String storedPasswordHash) {
+//        try {
+//            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+//            byte[] hashedPassword = digest.digest(inputPassword.getBytes(StandardCharsets.UTF_8));
+//            String hashedPasswordHex = DatatypeConverter.printHexBinary(hashedPassword).toLowerCase();
+//
+//            return hashedPasswordHex.equals(storedPasswordHash.toLowerCase());
+//        } catch (NoSuchAlgorithmException e) {
+//            e.printStackTrace();
+//            return false;
+//        }
+        System.out.println("Debug: password from db: " + storedPasswordHash);
+        System.out.println("Debug: password from user: " + inputPassword);
+        if (inputPassword.equals(storedPasswordHash)){
+
+            return true;
+        }
+        return false;
+    }
 }

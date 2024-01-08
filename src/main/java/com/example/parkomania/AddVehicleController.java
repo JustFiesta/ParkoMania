517b3139247aefb5dc098a4508af274696e6a4b0
JustFiesta <mateusz.bocak9@gmail.com>
2024-01-08 23:38:33 +0100

    Adding vehicle functionality

--- src/main/java/com/example/parkomania/AddVehicleController.java
+++ src/main/java/com/example/parkomania/AddVehicleController.java
@@ -5,7 +5,15 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static com.example.parkomania.HelloApplication.loggedUser;
 import static com.example.parkomania.HelloApplication.sceneManager;
 
 public class AddVehicleController {
@@ -23,23 +31,61 @@ public class AddVehicleController {
     private Button goBack;
 
     @FXML
-    private Label passwordChangeSuccesful11;
-
-    @FXML
     private Button submitNewVehicle;
 
     @FXML
-    void checkVehicle(ActionEvent event) {
-
-    }
-
-    @FXML
     void submitNewVehicle(ActionEvent event) {
+        System.out.println("Debug: Given plate: " + checkVehicle.getText());
 
+        if (checkVehicle.getText().equals("") || !isRegistrationValid(checkVehicle.getText())){
+            addVehicleUnsuccesful.setVisible(true);
+            System.out.println("Debug: Blank or invalid plate!");
+        }else {
+            addVehicleUnsuccesful.setVisible(false);
+
+            SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+            Session session = sessionFactory.openSession();
+
+            Transaction transaction = null;
+
+            //try to send transaction to db
+            try {
+                transaction = session.beginTransaction();
+
+                Vehicle newVehicle = new Vehicle(0, checkVehicle.getText(), loggedUser);
+
+                session.save(newVehicle);
+
+                transaction.commit();
+                addVehicleSuccesful.setVisible(true);
+                checkVehicle.clear();
+            } catch (Exception e) {
+                if (transaction != null) {
+                    transaction.rollback();
+                }
+                System.out.println("Debug: Something went wrong");
+                e.printStackTrace();
+            } finally {
+                session.close();
+                sessionFactory.close();
+                System.out.println("Debug: Session close");
+            }
+        }
     }
+
     @FXML
     void goBack(ActionEvent event) {
         sceneManager.switchScene("userData");
+    }
+
+    public static boolean isRegistrationValid (String plateNumber) {
+        // Polski standard tablic rejestracyjnych: jedna lub dwie litery, a następnie od 5 do 7 cyfr
+        String plateRegex = "^[A-Z][A-Z0-9 ]*$";
+
+        Pattern pattern = Pattern.compile(plateRegex);
+        Matcher matcher = pattern.matcher(plateNumber);
+
+        return matcher.matches();
     }
 
 }

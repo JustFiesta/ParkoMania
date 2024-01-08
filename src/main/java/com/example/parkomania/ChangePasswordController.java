ee70eeb7092dc0543210aefbe81727e2e2b0e2c0
JustFiesta <mateusz.bocak9@gmail.com>
2024-01-08 23:41:14 +0100

    Changing password function

--- src/main/java/com/example/parkomania/ChangePasswordController.java
+++ src/main/java/com/example/parkomania/ChangePasswordController.java
@@ -6,13 +6,20 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
 
+import java.util.List;
+
+import static com.example.parkomania.HelloApplication.loggedUser;
 import static com.example.parkomania.HelloApplication.sceneManager;
 
 public class ChangePasswordController {
 
     @FXML
-    private TextField checkPhoneNumber;
+    private TextField checkPassword;
 
     @FXML
     private Button goBack;
@@ -31,21 +38,47 @@ public class ChangePasswordController {
 
     @FXML
     private Button submitNewPassword;
-
     @FXML
-    void checkPhoneNumber(ActionEvent event) {
-
-    }
-
-
-    @FXML
-    void newPassword(ActionEvent event) {
-
-    }
+    private Label incorrectPassword;
 
     @FXML
     void submitNewPassword(ActionEvent event) {
+        //check if password is correct
 
+        //if password is empty do not check db
+        if (checkPassword.getText().equals("") || checkPassword.getText().equals(newPassword.getText())){
+            incorrectPassword.setVisible(true);
+            System.out.println("Debug: Wrong password! It is either empty or same as old one");
+            newPassword.clear();
+        }else {
+            incorrectPassword.setVisible(false);
+
+            SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+            Session session = sessionFactory.openSession();
+
+            Transaction transaction = null;
+
+            //try to send transaction to db
+            try {
+                transaction = session.beginTransaction();
+                loggedUser.setPassword_hash(newPassword.getText());
+                session.merge(loggedUser);
+
+                transaction.commit();
+                checkPassword.clear();
+                newPassword.clear();
+            } catch (Exception e) {
+                if (transaction != null) {
+                    transaction.rollback();
+                }
+                System.out.println("Debug: Something went wrong");
+                e.printStackTrace();
+            } finally {
+                session.close();
+                sessionFactory.close();
+                System.out.println("Debug: Session close");
+            }
+        }
     }
     @FXML
     void goBack(ActionEvent event) {
